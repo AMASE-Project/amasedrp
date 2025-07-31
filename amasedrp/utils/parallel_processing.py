037cba15a9a79739b95a5cfceb545b9e609e36c2
Zeng Guangquan <guangquan.zeng@outlook.com>
2025-07-31 11:21:23 +0800

    [utils] Add some parallel processing helper functions.

---
+++ amasedrp/utils/parallel_processing.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@File:         parallel.py
+@Author:       Guangquan ZENG
+@Contact:      guangquan.zeng@outlook.com
+@Description:  Some parallel utilities.
+'''
+
+
+from joblib import parallel_backend
+from joblib import Parallel, delayed
+
+
+def run(function, inputs, parallel=True, n_jobs=-1, backend='loky'):
+    """
+    >> output = function(input)
+    # the "input" represents one or multiple positional arguments.
+
+    >> outputs = parallel_function(function, inputs)
+    # inputs = [input_0, input_1, ...]
+    # outputs = [output_0, output_1, ...]
+    """
+    if parallel:
+        with parallel_backend(backend=backend, n_jobs=n_jobs):
+            try:
+                outputs = Parallel()(
+                    delayed(function)(*input) for input in inputs)
+            except TypeError:
+                outputs = Parallel()(
+                    delayed(function)(input) for input in inputs)
+    else:
+        try:
+            outputs = [function(*input) for input in inputs]
+        except TypeError:
+            outputs = [function(input) for input in inputs]
+    return outputs
