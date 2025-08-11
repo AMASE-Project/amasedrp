697172d88ca47a9073d76642e7807c515da751d7
Zeng Guangquan <guangquan.zeng@outlook.com>
2025-08-11 17:55:07 +0800

    [LSF fitting] Improve the LSF fitting function.

--- amasedrp/reduction/functions/lsf_fitting.py
+++ amasedrp/reduction/functions/lsf_fitting.py
@@ -150,5 +150,6 @@ def lsf_fitting(spectrum, spectrum_wls, target_wl):
     target_fwhm, _, _, _, _ = lsf_gaussian_fitting(
         spectrum, spectrum_wls, target_wl,
         cutout_wl_half_width=5.,
+        adjust_target_wl=True,
     )
     return target_fwhm
